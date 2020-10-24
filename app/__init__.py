eb1266f7dc5527e763414aee16ca82de6a92e5d8
sawe-daisy <deesawe16@gmail.com>
2020-10-25 01:55:26 +0300

    login of users

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,10 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    #registration of blueprint instances
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
