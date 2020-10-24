2fb41c89e04cad4fa53a4aa1179fb9560bbc474c
sawe-daisy <deesawe16@gmail.com>
2020-10-25 01:45:28 +0300

    user authentication

--- app/models.py
+++ app/models.py
@@ -1,11 +1,26 @@
 from . import db
 from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(255))
+    pass_word= db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_word,password)
+
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User 
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
 app = create_app('development')
 
-manager = Manager(app) 
+manager = Manager(app)
+#initializing migrate class 
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
 
 @manager.shell
