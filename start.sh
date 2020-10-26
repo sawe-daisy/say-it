3f14aabf403a872146a46c2a9f42c3a19ed99163
sawe-daisy <deesawe16@gmail.com>
2020-10-26 21:29:31 +0300

    email

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-MAIL_USERNAME=daickate@gmail.com
-MAIL_PASSWORD=daickareen
-SECRET_KEY =12345sawe###%%%%
+MAIL_USERNAME=christiansawe16@gmail.com
+MAIL_PASSWORD=~3og3YXm)#([yG@)I}La7(r1>fQ&7>
+SECRET_KEY =~3og3YXm)#([yG@)I}La7(r1>fQ&7>
 python3.8 manage.py server
