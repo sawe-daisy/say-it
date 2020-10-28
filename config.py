3c5e276370252fbd4b4c1c9cf7e65dba7d8196c6
sawe-daisy <deesawe16@gmail.com>
2020-10-28 11:29:21 +0300

    deploy

--- config.py
+++ config.py
@@ -19,6 +19,21 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # DEBUG=True
+    # MAIL_USE_SSL =True
+    MAIL_USE_TLS = True
+    MAIL_MAX_EMAILS=None
+    MAIL_ASCII_ATTACHMENTS = False
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class TestConfig(Config):
     pass
